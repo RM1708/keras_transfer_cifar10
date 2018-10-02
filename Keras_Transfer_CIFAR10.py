cfa468e0bf17fd16aad6c2b705957ea50ac2ec85
RM <aaremnair@gmail.com>
2018-10-02 10:19:43 +0530

    deleted:    .gitignore
    	modified:   Keras_Transfer_CIFAR10.py
    	1. Added print statements for showing progress.

--- Keras_Transfer_CIFAR10.py
+++ Keras_Transfer_CIFAR10.py
@@ -1,4 +1,6 @@
-
+'''
+https://alexisbcook.github.io/2017/using-transfer-learning-to-classify-images-with-keras/
+'''
 # coding: utf-8
 
 # # Extract Bottleneck Features for Train Set
@@ -37,16 +39,19 @@ print('data loaded')
 
 
 
-# load inceptionV3 model + remove final classification layers
-model = InceptionV3(weights='imagenet', include_top=False, input_shape=(139, 139, 3))
+# load inceptionV3 model + 
+#remove final classification layers by setting include_top to False
+model = InceptionV3(weights='imagenet', \
+                    include_top=False, \
+                    input_shape=(139, 139, 3))
 print('model loaded')
 
-# obtain bottleneck features (train)
+# obtain bottleneck features_train
 if os.path.exists('inception_features_train.npz'):
-    print('bottleneck features detected (train)')
-    features = np.load('inception_features_train.npz')['features']
+    print('bottleneck features_train detected (train). Loading features_train ...')
+    features_train = np.load('inception_features_train.npz')['features']
 else:
-    print('bottleneck features file not detected (train)')
+    print('bottleneck features_train file not detected (train)')
     print('calculating now ...')
     # pre-process the train data
     tmp = []
@@ -62,11 +67,11 @@ else:
 #                            for i in range(0, len(x_train))]).astype('float32')
     inception_input_train = preprocess_input(big_x_train)
     print('train data preprocessed')
-    # extract, process, and save bottleneck features
-    features = model.predict(inception_input_train)
-    features = np.squeeze(features)
-    np.savez('inception_features_train', features=features)
-print('bottleneck features saved (train)')
+    # extract, process, and save bottleneck features_train
+    features_train = model.predict(inception_input_train)
+    features_train = np.squeeze(features_train)
+    np.savez('inception_features_train', features=features_train)
+    print('bottleneck features_train saved')
 
 
 # # Plot t-SNE Embedding of Bottleneck Features for Train Set
@@ -78,20 +83,26 @@ from tsne import bh_sne
 import matplotlib.pyplot as plt
 #get_ipython().run_line_magic('matplotlib', 'inline')
 
-# reshape bottleneck features + reduce dimensionality with t-SNE
-if os.path.exists('tsne_features.npz'):
-    print('tsne features detected (test)')
-    tsne_features = np.load('tsne_features.npz')['tsne_features']
+# reshape bottleneck features_train + reduce dimensionality with t-SNE
+if os.path.exists('tsne_features_train.npz'):
+    print('tsne features detected (test). Loading t-SNE features ...')
+    tsne_features_train = np.load('tsne_features_train.npz')['tsne_features']
 else:
     print('tsne features not detected (test)')
     print('calculating now ...')
-    tsne_features = bh_sne(features.reshape([features.shape[0], np.prod(features.shape[1:])]).astype('float64')[:25000])
-    np.savez('tsne_features', tsne_features=tsne_features)
-print('tsne features obtained')
+    tsne_features_train = \
+                bh_sne(features_train.reshape([features_train.shape[0], \
+                                             np.prod(features_train.shape[1:])]).astype('float64')[:25000])
+    np.savez('tsne_features_train', tsne_features=tsne_features_train)
+    print('tsne features obtained')
 
 # plot the features
 plt.figure(figsize=(20,20))
-plt.scatter(tsne_features[:,0], tsne_features[:,1], c=plt.cm.jet(y_train/10), s=10, edgecolors='none')
+plt.scatter(tsne_features_train[:,0], \
+            tsne_features_train[:,1], \
+            c=plt.cm.jet(y_train/10), \
+            s=10, \
+            edgecolors='none')
 plt.show()
 
 
@@ -102,10 +113,10 @@ plt.show()
 
 # obtain bottleneck features (test)
 if os.path.exists('inception_features_test.npz'):
-    print('bottleneck features detected (test)')
+    print('bottleneck features_test detected. Loading features_test ...')
     features_test = np.load('inception_features_test.npz')['features_test']
 else:
-    print('bottleneck features file not detected (test)')
+    print('bottleneck features_test file not detected')
     print('calculating now ...')
     # pre-process the test data
     big_x_test = np.array([scipy.misc.imresize(x_test[i], (139, 139, 3)) 
@@ -115,7 +126,7 @@ else:
     features_test = model.predict(inception_input_test)
     features_test = np.squeeze(features_test)
     np.savez('inception_features_test', features_test=features_test)
-print('bottleneck features saved (test)')
+    print('bottleneck features_test saved')
 
 
 # # Train a Shallow NN
@@ -136,13 +147,16 @@ y_test = np_utils.to_categorical(y_test, 10)
 from keras.callbacks import ModelCheckpoint   
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Conv2D, GlobalAveragePooling2D
-
+#/home/rm/anaconda3/envs/tensorflow/lib/python3.6/site-packages/keras/engine/sequential.py
 model = Sequential()
-model.add(Conv2D(filters=100, kernel_size=2, input_shape=features.shape[1:]))
+model.add(Conv2D(filters=100, \
+                 kernel_size=2, \
+                 input_shape=features_train.shape[1:]))
 model.add(Dropout(0.4))
 model.add(GlobalAveragePooling2D())
 model.add(Dropout(0.3))
-model.add(Dense(10, activation='softmax'))
+model.add(Dense(10, \
+                activation='softmax'))
 model.summary()
 
 model.compile(loss='categorical_crossentropy', \
@@ -151,7 +165,7 @@ model.compile(loss='categorical_crossentropy', \
 
 checkpointer = ModelCheckpoint(filepath='model.best.hdf5', 
                                verbose=1, save_best_only=True)
-model.fit(features, y_train, batch_size=50, epochs=50,
+model.fit(features_train, y_train, batch_size=50, epochs=50,
           validation_split=0.2, callbacks=[checkpointer],
           verbose=2, shuffle=True)
 
